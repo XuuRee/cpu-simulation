fe3476d154410e17535d7975134b9d1b8460ac9a
Filip Valchář <xvalchar@fi.muni.cz>
2017-04-01 12:32:35 +0200

    listInit, listPush, listClear

--- cpu-simulation/instruction_list.c
+++ cpu-simulation/instruction_list.c
@@ -1,15 +1,76 @@
 #include "instruction_list.h"
+#include <stdlib.h>
 
+/*
+ * Inicializovat vsechny ostatni argumenty na NULL nebo 0 ?
+ */
 void listInit(struct instructionList* container) {
-
+    container->current = NULL;
+    container->end = NULL;
 }
 
+
+const char* getInstruction(enum instructionType instruction) {
+    switch (instruction) {
+        case InstNop: return "InstNop";
+        case InstAdd: return "InstAdd";
+        case InstSub: return "InstSub";
+        case InstMul: return "InstMul";
+        case InstDiv: return "InstDiv";
+        case InstMova: return "InstMova";
+        case InstLoad: return "InstLoad";
+        case InstSwac: return "InstSwac";
+        case InstSwab: return "InstSwab";
+        case InstInc: return "InstInc";
+        case InstDec: return "InstDec";
+        case InstPush: return "InstPush";
+        case InstPop: return "InstPop";
+        default: return "Unknown value";
+   }
+}
+
+
+void printList(struct instructionList* container) {
+    struct instruction *currentNode = container->current;
+
+    while (currentNode != NULL) {
+        printf("%s\n", getInstruction(currentNode->type));
+        currentNode = currentNode->next;
+    }
+}
+
+
 unsigned int listClear(struct instructionList* container) {
-    return 0;
+    struct instruction *currentNode = container->current;
+    struct instruction *nextNode;
+    unsigned int lengthList = 0;
+
+    while (currentNode->next != NULL) {
+        nextNode = currentNode->next;
+        free(currentNode);
+        currentNode = nextNode;
+        lengthList++;
+    }
+
+    free(currentNode);
+    container->current = NULL;
+    container->end = NULL;
+
+    return lengthList + 1;      // posledni, bez zarazky, tzn lengthList - 1
 }
 
 void listPush(struct instructionList* container, struct instruction* item) {
-
+    if (container->current == NULL) {      // tzn. is NULL ?
+        container->current = item;
+        container->end = item;
+        item->next = NULL;                  // has to be NULL?
+        item->prev = NULL;
+    } else {
+        container->end->next = item;
+        item->next = NULL;
+        item->prev = container->end;
+        container->end = item;
+    }
 }
 
 const struct instruction * listStep(struct instructionList* container) {
--- cpu-simulation/main.c
+++ cpu-simulation/main.c
@@ -1,3 +1,52 @@
+#include "instruction_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 int main() {
+    printf("Program is now starting...\n");
+
+    struct instructionList *list;
+    struct instruction *firstInstr;
+    struct instruction *secondInstr;
+
+    list = malloc(sizeof(list));
+
+    if (!list) {
+        printf("Nepodarilo se alokovat pamet!\n");
+        return 1;
+    }
+
+    firstInstr = malloc(sizeof(firstInstr));
+
+    if (!firstInstr) {
+        printf("Nepodarilo se alokovat pamet!\n");
+        return 1;
+    }
+
+    secondInstr = malloc(sizeof(secondInstr));
+
+    if (!secondInstr) {
+        printf("Nepodarilo se alokovat pamet!\n");
+        return 1;
+    }
+
+    firstInstr->arg = 1;
+    firstInstr->type = InstAdd;
+    firstInstr->next = NULL;
+    firstInstr->prev = NULL;
+
+    secondInstr->arg = 1;
+    secondInstr->type = InstSub;
+    secondInstr->next = NULL;
+    secondInstr->prev = NULL;
+
+    listInit(list);
+    listPush(list, firstInstr);
+    listPush(list, secondInstr);
+    printList(list);
+    printf("Delete items: %d\n", listClear(list));
+    free(list);
+
 	return 0;
 }
